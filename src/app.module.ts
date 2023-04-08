b6ef85b00161f5c5cedc1a0470ddb20914bda07a
HironoriSato <maza.hs5360@gmail.com>
2023-04-09 00:05:52 +0900

    fix: app.moduleに新規作成したserviceを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService, AppService, UserService, PostService],
 })
 export class AppModule {}
